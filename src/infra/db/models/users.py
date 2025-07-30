816af7c02fe897b5296162f5497b3984c7e6d0ed
eumozge <eumozge@gmail.com>
2025-07-30 20:25:33 +0300

    [feat] [infra] add sqlalchemy user model

---
+++ src/infra/db/models/users.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from uuid import uuid4
+
+import sqlalchemy as sa
+from domain.users import value_objects
+from infra.db.models.base import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    id: Mapped[value_objects.UserId] = mapped_column(
+        sa.UUID,
+        primary_key=True,
+        default=uuid4,
+        server_default=sa.func.uuid_generate_v4(),
+    )
+    username: Mapped[value_objects.Username] = mapped_column(sa.String, unique=True)
+    created_at: Mapped[datetime] = mapped_column(nullable=False, server_default=sa.sql.func.now())
