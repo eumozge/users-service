6ffb99d309f30ce492eadd143c6c1b56577bbd41
eumozge <eumozge@gmail.com>
2025-08-01 08:19:43 +0300

    [fix] [infra] annotations for test user factories

--- src/infra/db/tests/factories.py
+++ src/infra/db/tests/factories.py
@@ -1,23 +1,23 @@
 import uuid
-from typing import Any, Self, TypeVar
+from typing import Any, Generic, TypeVar
 
 import factory
 from factory.alchemy import SQLAlchemyModelFactory
 from infra.db.models.users import UserModel
 
-T = TypeVar("T", bound="AsyncSQLAlchemyModelFactory")
+T = TypeVar("T")
 
 
-class AsyncSQLAlchemyModelFactory(SQLAlchemyModelFactory):
+class AsyncSQLAlchemyModelFactory(SQLAlchemyModelFactory, Generic[T]):
     @classmethod
-    async def _create(cls, model_class: Any, *args: Any, **kwargs: Any) -> Self:
+    async def _create(cls, model_class: type[T], *args: Any, **kwargs: Any) -> T:
         instance = super()._create(model_class, *args, **kwargs)
         async with cls._meta.sqlalchemy_session as session:
             await session.commit()
         return instance
 
 
-class UserFactory(AsyncSQLAlchemyModelFactory):
+class UserFactory(AsyncSQLAlchemyModelFactory[UserModel]):
     id = factory.LazyFunction(uuid.uuid4)
     username = factory.Faker("user_name")
 
